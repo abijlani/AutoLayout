2841c7243c87e31dc94c3aa2adf3f4432c9f42a0
Amit Bijlani <amit@teamtreehouse.com>
2014-02-06 16:13:03 -0500

    Added constraints using Visual Format Language

--- AutoLayout/THViewController.m
+++ AutoLayout/THViewController.m
@@ -17,7 +17,54 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    [self addConstraints];
+}
+
+- (void) addConstraints {
+    
+    [self.view removeConstraints:self.view.constraints];
+    
+    
+    UILabel *firstName = self.firstName;
+    UILabel *lastName = self.lastName;
+    UIImageView *imageView = self.imageView;
+    UITextView *comment = self.textView;
+    
+    NSDictionary *views = NSDictionaryOfVariableBindings(firstName,lastName,imageView,comment);
+    
+    NSDictionary *metrics = @{@"width": @160.0, @"horizontalSpacing":@15.0, @"verticalSpacing":@10};
+    
+    NSArray *constraints = [NSLayoutConstraint constraintsWithVisualFormat:
+                            @"H:|-[imageView(100)]-horizontalSpacing-[firstName(>=width)]-|"
+                                                                   options:NSLayoutFormatAlignAllTop
+                                                                   metrics:metrics
+                                                                     views:views];
+    
+    constraints = [constraints arrayByAddingObjectsFromArray:
+                   [NSLayoutConstraint constraintsWithVisualFormat:
+                    @"H:[imageView]-horizontalSpacing-[lastName(>=width)]-|"
+                                                           options:0
+                                                           metrics:metrics
+                                                             views:views]];
+
+    constraints = [constraints arrayByAddingObjectsFromArray:
+                   [NSLayoutConstraint constraintsWithVisualFormat:
+                    @"H:[imageView]-horizontalSpacing-[comment(>=width)]-|"
+                                                           options:NSLayoutFormatAlignAllBottom
+                                                           metrics:metrics
+                                                             views:views]];
+    
+    constraints = [constraints arrayByAddingObjectsFromArray:
+                   [NSLayoutConstraint constraintsWithVisualFormat:
+                    @"V:|-50-[firstName]-verticalSpacing-[lastName]-verticalSpacing-[comment]"
+                                                           options:0
+                                                           metrics:metrics
+                                                             views:views]];
+
+
+    [self.view addConstraints:constraints];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,3 +74,14 @@
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
